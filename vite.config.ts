8e085050787f74ca734b87ebfb0ba97ab198c523
Kasper Guldmann Nielsen <git@kalleguld.dk>
2022-07-16 13:24:13 +0200

    change entry point filename

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react'
 import { viteSingleFile } from "vite-plugin-singlefile"
 
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()]
+  plugins: [react(), viteSingleFile()],
+  build:{
+    rollupOptions:{
+      input:{
+        app: "browse.html"
+      }
+    }
+  }
 })
