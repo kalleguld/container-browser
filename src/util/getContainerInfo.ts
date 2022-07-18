3b383ad5df0884b41587b7864b8292792b600800
Kasper Guldmann Nielsen <kgn@techsoft.dk>
2022-07-18 15:50:46 +0200

    fix header

--- src/util/getContainerInfo.ts
+++ src/util/getContainerInfo.ts
@@ -1,8 +1,7 @@
 
 export function getContainerInfo(): ContainerInfo | undefined {
     const regex = /^https\:\/\/(.*?)\.blob\.core\.windows\.net\/(.*?)[\/\?]/;
-    //const href = window.location.href;
-    const href = "https://kgnstorage.blob.core.windows.net/athco/browse.html#/archive";
+    const href = window.location.href;
     const match = regex.exec(href);
     if (!match)
         return;
